eff8f0b8f27f7e052f0dde92c71dba3931b5457f
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-05-13 23:34:17 +0700

    V1.2

--- Assets/Script/Huy/Player/PlayerController.cs
+++ Assets/Script/Huy/Player/PlayerController.cs
@@ -6,25 +6,68 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
 
-    Vector2 moveInput;
-    Rigidbody2D myRigidbody;
+    private Vector2 moveInput;
+    private Rigidbody2D myRigidbody;
+    private SpriteRenderer mySpriteRenderer;
+    private Animator myAnimator;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
 
     }
 
     void Update()
     {
         Run();
+        Flip();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+
+        Animation();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            // do stuff
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Animation()
+    {
+        if (moveInput.x != 0)
+        {
+            myAnimator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("IsRunning", false);
+        }
+        
+    }
+
+    void Flip()
+    {
+        if (moveInput.x == -1)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else if (moveInput.x == 1)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+       
     }
 
     void Run()
