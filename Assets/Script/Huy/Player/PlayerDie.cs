3963a96e84a19ec5169db2c9ec3201849b93cfab
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-05-19 15:26:16 +0700

    V1.6.3

--- Assets/Script/Huy/Player/PlayerDie.cs
+++ Assets/Script/Huy/Player/PlayerDie.cs
@@ -24,9 +24,8 @@ public class PlayerDie : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject.tag == "Water" || collision.gameObject.tag == "Trap" || collision.gameObject.tag == "Enemy")
         {
             PlayerDied();
