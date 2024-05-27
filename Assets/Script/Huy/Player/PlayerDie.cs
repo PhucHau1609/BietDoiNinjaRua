7c46a12d8309ee518cc58151592c3a08b2b3b5c2
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-05-27 21:40:51 +0700

    V1.8.6
    fix heart bar.

--- Assets/Script/Huy/Player/PlayerDie.cs
+++ Assets/Script/Huy/Player/PlayerDie.cs
@@ -5,51 +5,67 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDie : MonoBehaviour
 {
-    [SerializeField] GameObject Player;
+    [SerializeField] GameObject player;
+    [SerializeField] Animator aniPlayer;
+    [SerializeField] float timeReTakeDamage;
     [SerializeField] float timeLoadGameOverPlayerDied;
+    [SerializeField] CapsuleCollider2D capsuleCollider;
 
     private Vector2 startCheckPoint;
+    bool isTouchTrap;
 
     private void Start()
     {
-        startCheckPoint = Player.transform.position;
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        startCheckPoint = player.transform.position;
     }
     private void Update()
     {
-        
         if (GameManager.Instance.GetHeart() <= 0)
         {
             StartCoroutine(GameOver());
         }
+
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Water" || collision.gameObject.tag == "Trap" || collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Trap"
+            || collision.gameObject.tag == "Water"
+            || collision.gameObject.tag == "Enemy")
         {
-            PlayerDied();
+            StartCoroutine(PlayerReceiveDamage());
+            isTouchTrap = true;
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            isTouchTrap = false;
         }
     }
 
-    private void PlayerDied()
+    private IEnumerator PlayerReceiveDamage()
     {
-        //FindObjectOfType<AudioManager>().Play("PlayerDeath");
-        //StartCoroutine(WayPlayerDied());
 
-        Player.transform.position = startCheckPoint;
-
-        if (GameManager.Instance.GetHeart() >= 1)
+        Debug.Log("Touch Trap: " + isTouchTrap);
+        while (isTouchTrap)
         {
-            GameManager.Instance.SetHeart(GameManager.Instance.GetHeart() - 1);
+            Debug.Log("Is Work");
+            GameManager.Instance.ReceiveDamage(1);
+            aniPlayer.SetBool("ReceiveDamage", true);
+            yield return new WaitForSeconds(timeReTakeDamage);
+            aniPlayer.SetBool("ReceiveDamage", false);
         }
     }
+
+
 
     private IEnumerator GameOver()
     {
         //crossFadeNextLevel.SetTrigger("Start");
         //FindObjectOfType<AudioManager>().Play("PlayerDeath");
         yield return new WaitForSeconds(timeLoadGameOverPlayerDied);
-        Destroy(Player);
+        Destroy(player);
         SceneManager.LoadScene("GameOver");
     }
 }
