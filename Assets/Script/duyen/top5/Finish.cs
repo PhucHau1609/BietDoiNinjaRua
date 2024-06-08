08b6e555cccf4e65688312a1c9faeea1157761b9
Duyenmi <duyenntmps39464@gmail.com>
2024-06-08 21:59:57 +0700

    86243

--- Assets/Script/duyen/top5/Finish.cs
+++ Assets/Script/duyen/top5/Finish.cs
@@ -5,6 +5,7 @@ public class Finish : MonoBehaviour
 {
     private StorageHelper storageHelper;
     private gamedataplay played;
+    [SerializeField] GameObject row;
 
     void Start()
 {
@@ -40,10 +41,23 @@ public class Finish : MonoBehaviour
                     timePlayer = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") // Corrected the format
                 };
                 played.plays.Add(game_data);
-                // storageHelper.SaveData();
-                // storageHelper.LoadData();
-                // played = storageHelper.played;
+                storageHelper.SaveData();
+                storageHelper.LoadData();
+                played = storageHelper.played;
                 Debug.Log("Count: " + played.plays.Count);
+                //xx
+            played.plays.Sort((x,y) => y.Coint.CompareTo(x.Coint));
+            var plays = played.plays.GetRange(0,Math.Min(5, played.plays.Count));
+            //loadbar
+            for (int i = 0; 1 < plays.Count; i++)
+            {
+                var rowInstance = Instantiate(row, row.transform.parent);
+                rowInstance.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = (i + 1).ToString();
+                rowInstance.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = plays[i].Coint.ToString();
+                rowInstance.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = plays[i].timePlayer;
+                rowInstance.SetActive(true);
+                Destroy(gameObject);
+            }
             }
             else
             {
